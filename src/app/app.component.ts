4870bb9e8e51e997879cbddb06b5f11cec24e545
allyusd <allyusd@yahoo.com.tw>
2016-10-31 22:35:10 +0800

    Echo WebSocket Test Ref: http://www.websocket.org/echo.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  private title = 'app works!';
+  private wsUri = 'ws://localhost:8812';
+  private websocket:WebSocket = null;
+
+  public test(evt) {
+    this.websocket = new WebSocket(this.wsUri);
+    this.websocket.onopen = this.onOpen;
+    this.websocket.onclose = this.onClose;
+    this.websocket.onmessage = this.onMessage;
+    this.websocket.onerror = this.onError;
+  }
+
+  private onOpen = (ev: Event): any => {
+    console.log("CONNECTED");
+    this.doSend("WebSocket rocks");
+  }
+
+  private onClose = (ev: CloseEvent): any => {
+    console.log("DISCONNECTED");
+  }
+
+  private onMessage = (ev: MessageEvent): any => {
+    console.log('RESPONSE: ' + ev.data);
+    this.websocket.close();
+  }
+
+  private onError = (ev: ErrorEvent): any => {
+    console.log('ERROR: ' + ev.error);
+  }
+
+  private doSend(message) {
+    console.log("SENT: " + message);
+    this.websocket.send(message);
+  }
 }
